89f6b3e183a2b0bfd6e7c6a08bb34a5a91594a1c
sunlintong <sunlintong@ghostcloud.cn>
2018-03-23 13:46:33 +0800

    s

--- controllers/image_controller.go
+++ controllers/image_controller.go
@@ -32,7 +32,7 @@ func (ic *ImageController) GetAllImages() {
 		for _, rt := range image.RepoTags {
 			var data [4]string
 			data[0] += rt
-			data[1] = string([]byte(image.ID)[:20])
+			data[1] = image.ID
 			data[2] = ic.GetTimeString(image.Created)
 			data[3] = fmt.Sprintf("%.2f", float64(image.Size)/1000000) + " MB"
 			ret = append(ret, data)

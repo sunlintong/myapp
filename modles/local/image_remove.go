28b82e8d7c4d1fa55887453f431f35cfc9c835e5
sunlintong <sunlintong@ghostcloud.cn>
2018-03-23 19:09:12 +0800

    s

--- modles/local/image_remove.go
+++ modles/local/image_remove.go
@@ -17,7 +17,7 @@ func ForceRemoveImage(image_id string) ([]types.ImageDeleteResponseItem, error) 
 	cli := GetClient()
 	ctx := context.Background()
 	options := types.ImageRemoveOptions{}
-	options.Force = false
-	items, err := cli.ImageRemove(ctx, image_id, types.ImageRemoveOptions{})
+	options.Force = true
+	items, err := cli.ImageRemove(ctx, image_id, options)
 	return items, err
 }

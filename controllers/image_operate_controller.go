5cd2a076601ebbe2664c75a75384efbbc5642571
sunlintong <sunlintong@ghostcloud.cn>
2018-04-13 14:31:30 +0800

    s

--- controllers/image_operate_controller.go
+++ controllers/image_operate_controller.go
@@ -46,7 +46,7 @@ func (ic *ImageController) GetAllImages() {
 	for _, image := range images {
 		for _, rt := range image.RepoTags {
 			var data [4]string
-			data[0] += rt
+			data[0] = rt
 			data[1] = image.ID
 			data[2] = ic.GetTimeString(image.Created)
 			data[3] = fmt.Sprintf("%.2f", float64(image.Size)/1000000) + " MB"
